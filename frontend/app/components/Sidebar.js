615dca13316a0a88576492471d367bd0c45a2c03
Sampath252005 <sampathnaik65@gmail.com>
2025-09-26 10:24:22 +0530

    created backend and frontend separate folder

---
+++ frontend/app/components/Sidebar.js
@@ -0,0 +1,72 @@
+"use client";
+import React, { useState } from "react";
+import {
+  LayoutDashboard,
+  FlaskConicalIcon,
+  ClipboardMinus,
+  Settings,
+  Menu,
+  X,
+} from "lucide-react";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <>
+      {/* Toggle Button (Always Visible) */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`fixed top-4  z-50 ${isOpen?'bg-black left-50':'bg-gray-800 left-4'} text-white p-2 rounded-lg`}
+      >
+        {isOpen ? <X size={22} /> : <Menu size={22} />}
+      </button>
+
+      {/* Sidebar */}
+      <aside
+        className={`${
+          isOpen ? "translate-x-0" : "-translate-x-64"
+        } w-64 bg-gray-800 p-4 flex flex-col h-screen transition-transform duration-300 fixed top-0 left-0 z-40`}
+      >
+        <h1 className="text-2xl  font-bold text-white mb-6 ">RailFlow AI</h1>
+
+        <nav className="flex flex-col gap-3">
+          <span className="flex space-x-3 items-center cursor-pointer p-2 rounded-2xl hover:bg-black">
+            <LayoutDashboard size={18} />
+            <a href="/" className="hover:text-blue-400">
+              Dashboard
+            </a>
+          </span>
+          <span className="flex space-x-3 items-center cursor-pointer p-2 rounded-2xl hover:bg-black">
+            <FlaskConicalIcon size={18} />
+            <a href="/simulation" className="hover:text-blue-400">
+              Simulation
+            </a>
+          </span>
+          <span className="flex space-x-3 items-center cursor-pointer p-2 rounded-2xl hover:bg-black">
+            <ClipboardMinus size={18} />
+            <a href="/reports" className="hover:text-blue-400">
+              Reports
+            </a>
+          </span>
+          <span className="flex space-x-3 items-center cursor-pointer p-2 rounded-2xl hover:bg-black">
+            <Settings size={18} />
+            <a href="/settings" className="hover:text-blue-400">
+              Settings
+            </a>
+          </span>
+        </nav>
+      </aside>
+
+      {/* Overlay (click to close) */}
+      {isOpen && (
+        <div
+          onClick={() => setIsOpen(false)}
+          className="fixed inset-0 bg-transparent  bg-opacity-0 z-30"
+        ></div>
+      )}
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/app/page.js
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import MetricCard from "./components/MetricCard";
+import TrainRow from "./components/TrainRow";
+import RailwaySectionView from "./components/RailwaySectionView";
+
+export default function DashBoard() {
+  return (
+    <>
+      <div className="space-y-6">
+        {/* Header */}
+        <h2 className="text-3xl font-bold ml-10">Dashboard</h2>
+        <p className="text-gray-400">
+          Real-time overview of the railway network.
+        </p>
+
+        {/* Top Metrics */}
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <MetricCard
+            title="Network Throughput"
+            value="1,205 TU/hr"
+            change="+5.2%"
+          />
+          <MetricCard
+            title="Average Delay"
+            value="6.8 min"
+            change="-12.5%"
+            negative
+          />
+          <MetricCard title="Track Utilization" value="85%" change="+2.1%" />
+          <MetricCard title="Punctuality" value="92.3%" change="+1.8%" />
+        </div>
+
+        {/* Middle Section */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Railway Section View */}
+          <RailwaySectionView/>
+
+          {/* AI Recommendations */}
+          <div className="bg-gray-800 rounded-xl p-4">
+            <h3 className="text-lg font-semibold mb-3">AI Recommendations</h3>
+            <ul className="list-disc list-inside text-gray-300 space-y-2">
+              <li className="text-red-400">
+                Re-route FRT-745 via Loop Line B…
+              </li>
+              <li className="text-yellow-400">
+                Hold EXP-002 at Charlie Central…
+              </li>
+              <li className="text-green-400">Reduce speed for EXP-001…</li>
+              <li className="text-green-400">Advise crew of FRT-745…</li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Train Status */}
+        <div className="bg-gray-800 rounded-xl p-4 overflow-x-auto">
+          <h3 className="text-lg font-semibold mb-3">Train Status</h3>
+          <table className="w-full text-left">
+            <thead>
+              <tr className="text-gray-400">
+                <th className="p-2">Train ID</th>
+                <th className="p-2">Category</th>
+                <th className="p-2">Status</th>
+                <th className="p-2">Delay (min)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TrainRow id="EXP-001" cat="Express" status="On Time" delay="0" />
+              <TrainRow
+                id="FRT-745"
+                cat="Freight"
+                status="Delayed"
+                delay="15"
+              />
+              <TrainRow id="LCL-102" cat="Local" status="On Time" delay="0" />
+              <TrainRow
+                id="EXP-002"
+                cat="Express"
+                status="Stopped"
+                delay="45"
+              />
+              <TrainRow id="FRT-301" cat="Freight" status="Delayed" delay="5" />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
