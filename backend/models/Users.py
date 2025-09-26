7a6f24de2b44ed0ab90693a9083ee48d69a9aba9
Sushanth105 <veenaganiga96@gmail.com>
2025-09-26 10:32:36 +0530

    edited

--- backend/models/Users.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel,Field
-from typing import Optional
-
-class Users(SQLModel , table = True):
-    id: Optional[int] = Field(default=None , primary_key=True)
-    name: str
-    email : str = Field(unique=True , index=True)
-    password: Optional[str] = Field(default=None,nullable=True)
-    picture: Optional[str] = Field(default=None,nullable=True)
